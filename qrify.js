a700f35bbcd1cfad6853041522d2753e3f4052fa
David Coles <coles.david@gmail.com>
2011-05-09 16:20:42 +1000

    Added script by David Coles, a JS bookmarklet which converts selected text into a QR code.

---
+++ qrify.js
@@ -0,0 +1,22 @@
+// QRify
+// Author: David Coles
+
+var txt='';
+
+// Get the text of the current selection
+if (window.getSelection)
+{
+    txt = window.getSelection();
+}
+else if (document.getSelection)
+{
+    txt = document.getSelection();
+}
+else if (document.selection)
+{
+    txt = document.selection.createRange().text;
+}
+
+// Open the selection in a new window
+window.open("https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl="
+            + encodeURI(txt));
