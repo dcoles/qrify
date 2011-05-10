f667191e79b94c3aec199c4c6f9b0d51a44debb7
David Coles <coles.david@gmail.com>
2011-05-09 21:55:34 -0700

    Enclose in a function to avoid polluting the browser's Javascript namespace

--- qrify.js
+++ qrify.js
@@ -1,32 +1,38 @@
 // QRify
 // Author: David Coles
 
-var txt='';
-
-// Get the text of the current selection
-if (window.getSelection)
+// Use a function to avoid polluting the browser's global namespace
+function qrify()
 {
-    txt = window.getSelection();
-}
-else if (document.getSelection)
-{
-    txt = document.getSelection();
-}
-else if (document.selection)
-{
-    txt = document.selection.createRange().text;
-}
+    var txt='';
 
-// If txt is still empty, use the current document's URL instead
-if (txt == '')
-{
-    txt = document.location.href;
+    // Get the text of the current selection
+    if (window.getSelection)
+    {
+        txt = window.getSelection();
+    }
+    else if (document.getSelection)
+    {
+        txt = document.getSelection();
+    }
+    else if (document.selection)
+    {
+        txt = document.selection.createRange().text;
+    }
+
+    // If txt is still empty, use the current document's URL instead
+    if (txt == '')
+    {
+        txt = document.location.href;
+    }
+
+    // Open the selection in a new window
+    window.open("https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl="
+                + encodeURIComponent(txt));
+
+    // Ensure that clicking the bookmarklet does not redirect the current page
+    // (Tested in Firefox, not a problem in Chrome)
+    return undefined;
 }
-
-// Open the selection in a new window
-window.open("https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl="
-            + encodeURIComponent(txt));
-
-// Ensure that clicking the bookmarklet does not redirect the current page
-// (Tested in Firefox, not a problem in Chrome)
-undefined;
+// Actually call the function
+qrify();
